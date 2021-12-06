15e39e5e7682c9a0a591ce529374d6a03f421ff9
Plu0330 <90374667+Plu0330@users.noreply.github.com>
2021-12-06 16:26:52 +0900

    Add files via upload

--- Homework8.java
+++ Homework8.java
@@ -1,35 +1,43 @@
+import java.io.FileNotFoundException;
 import java.util.Map;
 import java.util.HashMap;
+import java.io.File;
 import java.util.Scanner;
+
 
 public class Homework8 {
     public static void main(String[] args) {
+        Map<String, String> Login = new HashMap<>();
 
-        Map<String, String> login = new HashMap<>();
+        try {
+            Scanner sc = new Scanner(new File("db.txt"));
+            while (sc.hasNext()) {
+                String UserID = sc.next();
+                String UserPassWord = sc.next();
+                Login.put(UserID, UserPassWord);
+            }
+            sc.close();
+        }
+        catch (Exception e) {e.printStackTrace();}
 
-        login.put("myId", "myPass");
-        login.put("myId2", "myPass2");
-        login.put("myId3", "myPass3");
-
-        System.out.println("id와 password를 입력해주세요");
-        System.out.printf("id : ");
+        System.out.println("id와 password 를 입력해주세요.");
+        System.out.println("id : ");
 
         Scanner sc1 = new Scanner(System.in);
+        String ID = sc1.next();
+        ID = ID.trim();
 
-        String id = sc1.next();
-        id = id.trim();
+        if (Login.containsKey(ID)) {
+            System.out.println("password : ");
 
-        if (login.containsKey(id)) {
-            System.out.printf("password : ");
+            sc1 = new Scanner(System.in);
+            String PassWord = sc1.next();
+            PassWord = PassWord.trim();
 
-            Scanner sc2 = new Scanner(System.in);
-            String password = sc2.next();
-            password = password.trim();
-
-            if (login.get(id).contentEquals(password)) {System.out.println("id와 비밀번호가 일치합니다.");}
+            if (Login.get(ID).contentEquals(PassWord)) {System.out.println("id와 비밀번호가 일치합니다.");}
             else {System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");}
-        }
 
+        }
         else {System.out.println("입력하신 id는 존재하지 않습니다.");}
     }
 }
